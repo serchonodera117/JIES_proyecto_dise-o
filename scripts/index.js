14df173f8d8d5349ee9bf5ab4d73fc5af70f2d33
Serch Onodera <sergio.lobo117@gmail.com>
2022-11-10 21:26:45 -0600

    Menu terminado

---
+++ scripts/index.js
@@ -0,0 +1,44 @@
+let fristOption = document.getElementById("1");
+let secondOption = document.getElementById("2");
+let thirdOption = document.getElementById("3");
+
+let fourthOption = document.getElementById("4");
+let fifthOption = document.getElementById("5");
+let sixthOption = document.getElementById("6");
+let seventhOption = document.getElementById("7");
+let eighthOption = document.getElementById("8");
+
+let myIframe = document.getElementById("div-iframe-container");
+
+
+function one(){
+ changeBtnColor(1);
+ myIframe.src = "../paginas/con_cual.html"
+}
+function two(){
+ changeBtnColor(2);
+ myIframe.src = "../paginas/pon_en_su_lugar.html"
+
+}
+function three(){
+ changeBtnColor(3);
+ myIframe.src = "../paginas/escribe.html"
+
+}
+
+function four(){changeBtnColor(4);}
+function five(){changeBtnColor(5);}
+function six(){changeBtnColor(6);}
+function seven(){changeBtnColor(7);}
+function eighth(){changeBtnColor(8);}
+
+function changeBtnColor(n){
+    fristOption.style.fontWeight= (n==1)?"bold":"normal";
+    secondOption.style.fontWeight= (n==2)?"bold":"normal";
+    thirdOption.style.fontWeight= (n==3)?"bold":"normal";
+    fourthOption.style.fontWeight= (n==4)?"bold":"normal";
+    fifthOption.style.fontWeight= (n==5)?"bold":"normal";
+    sixthOption.style.fontWeight= (n==6)?"bold":"normal";
+    seventhOption.style.fontWeight= (n==7)?"bold":"normal";
+    eighthOption.style.fontWeight= (n==8)?"bold":"normal";
+}
