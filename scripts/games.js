7a4573712cf9194413a24e04e31734ae40f11968
Serch Onodera <sergio.lobo117@gmail.com>
2022-11-19 00:57:27 -0600

    paginas

---
+++ scripts/games.js
@@ -0,0 +1,10 @@
+let instructions1= document.getElementById("instuctions1")
+let game=document.getElementById("game1")
+function btn_cual(){
+       instructions1.style.display="none";
+       game.style.display="block";
+}
+function btn_volver(){
+    instructions1.style.display="block";
+    game.style.display="none";
+};
